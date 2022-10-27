63d00edde3bdb7bda46f93f7ef5bc3a120370468
Lý Quang Toại <115530159+lyquangtoaidz@users.noreply.github.com>
2022-10-27 23:33:06 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ printf '\n\033[1;32m Tiến Hành Cài Python\n\n'
 #pkg install python
 #pip install --upgrade pip
 pip install requests
-curl -s https://lyquangtoai.dev/lyquangtoai.py -o $home/usr/bin/lyquangtoai
+curl -s https://lyquangtoai.dev/lyquangtoai -o $home/usr/bin/lyquangtoai
 chmod 777 $home/usr/bin/lyquangtoai
 
 printf '\n\033[1;32m Gõ \033[1;33mlyquangtoai \033[1;32mĐể Vào Tool <3\n\n'

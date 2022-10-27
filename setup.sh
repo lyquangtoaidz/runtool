c66a379acc0a9b1dde0b9160fbe8521a5f5bc30e
Lý Quang Toại <115530159+lyquangtoaidz@users.noreply.github.com>
2022-10-27 23:29:29 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+home='/data/data/com.termux/files/'
+printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
+termux-setup-storage
+#apt update
+#apt upgrade
+printf '\n\033[1;32m Tiến Hành Cài Python\n\n'
+#pkg install python
+#pip install --upgrade pip
+pip install requests
+curl -s https://lyquangtoai.dev/lyquangtoai.py -o $home/usr/bin/lyquangtoai
+chmod 777 $home/usr/bin/lyquangtoai
+
+printf '\n\033[1;32m Gõ \033[1;33mlyquangtoai \033[1;32mĐể Vào Tool <3\n\n'
